e08273b342d02005fbcb7a7766c74c1c541699fb
caroliinaasilva <carolinadesousa84@yahoo.com>
2022-07-16 15:31:07 -0300

    adiciona env

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 const express = require('express')
-const cors = require ('cors')
+const cors = require('cors')
 const mongoose = require('./database/mongooseConnect')
 const swaggerUi = require('swagger-ui-express');
 const swaggerFile = require('../swagger/swagger_output.json');
@@ -8,9 +8,6 @@ const pacientesRoutes = require('./routes/pacientesRoutes')
 
 
 const app = express()
-
-
-
 
 app.use(express.json())
 app.use(cors())
