ca5828162952a13781490597b14a7fa70cf31a32
caroliinaasilva <carolinadesousa84@yahoo.com>
2022-07-23 11:10:49 -0300

    :bug: adicionando rota raiz

--- src/app.js
+++ src/app.js
@@ -3,7 +3,8 @@ const express = require('express')
 const cors = require('cors')
 const mongoose = require('./database/mongooseConnect')
 const swaggerUi = require('swagger-ui-express');
-const swaggerFile = require('../swagger/swagger_output.json');
+const swaggerFile = require('../swagger/swagger_output.json')
+const index = require('./routes/index')
 const pacientesRoutes = require('./routes/pacientesRoutes')
 const psicologasRoutes = require('./routes/psicologasRoutes')
 const userRoutes = require('./routes/userRoute')
@@ -16,7 +17,7 @@ app.use(cors())
 
 
 mongoose.connect()
-
+app.use('/', index)
 app.use(pacientesRoutes)
 app.use(psicologasRoutes)
 app.use(userRoutes)
