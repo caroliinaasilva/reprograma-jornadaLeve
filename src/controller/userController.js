eb0463a33fde9092b6b73262b71842e146dd1d2e
caroliinaasilva <carolinadesousa84@yahoo.com>
2022-07-25 10:06:20 -0300

    :sparkles: feat: Adicionando rotas de login

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,20 +1,66 @@
 const User = require ('../models/userModel')
+const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
+const SECRET = process.env.SECRET
 
 
 const create = (req, res) => {
-    const senhaComHash = bcrypt.hashSync(req.body.senha, 10);
-    req.body.senha = senhaComHash;
-    const newUser = new User(req.body);
+    const senhaComHash = bcrypt.hashSync(req.body.senha, 10)
+    req.body.senha = senhaComHash
 
-    newUser.save(function (err) {
-        if (err) {
-            res.status(500).send({ message: err.message })
-        }
-
-        res.status(201).send(newUser.toJSON())
+    const novoCadastro = new User(req.body)
+    novoCadastro.save(function (err) {
+      if (err) {
+        res.status(500).send({message: err.message})
+      }
+      res.status(201).send(novoCadastro)
     })
-};
+  }
 
+
+  const getAll = (req, res) => {
+    User.find(function (err, colaboradoras){
+        if(err){
+            res.status(500).send({message: err.message})
+        }
+        res.status(200).send(colaboradoras)
+    })
+}
+
+
+  const login = (req, res) => {
+    User.findOne({email: req.body.email}, function (error, user) {
+      if (error) {
+        return res.status(500).send({message: 'Não é possivel fazer o login'})
+      }
+      if (!user) {
+        res.status(404).send(`Não existe cadastro com esse email: ${req.body.email}`)
+      }
+
+      const passwordValid = bcrypt.compareSync(req.body.senha, user.senha)
+
+      if (!passwordValid) {
+        res.status(403).send('Senha não é válida')
+      }
+
+      const token = jwt.sign({email: req.body.email}, SECRET)
+       return res.status(200).send(token)
+    })
+
+  }
+  const deleteById = async (req, res) => {
+    try{
+        const { id } = req.params
+        await User.findByIdAndDelete(id)
+        res.status(200).json({message: `a user com o  ${id} foi deletada com sucesso`})
+    } catch (err) {
+        console.error(err)
+        res.status(500).json({message: err.message})
+    }
+}
 module.exports = {
-    create
+    create,
+    login,
+    getAll,
+    deleteById
 }
--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -2,7 +2,9 @@ const express = require("express");
 const router = express.Router();
 const controller = require('../controller/userController');
 
-router.post('/user/', controller.create)
-// router.post('/user/login', controller.login)
+router.post('/user', controller.create)
+router.post('/user/login', controller.login)
+router.get('/user', controller.getAll)
+router.delete('/user/:id', controller.deleteById)
 
 module.exports = router;
